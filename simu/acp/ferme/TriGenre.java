7250e0ff373b820c2ab3be4dd35442c0c101ea63
thamagicube <67897196+AmelieThai@users.noreply.github.com>
2026-01-01 23:49:07 +0100

    Update TriGenre.java

--- simu/acp/ferme/TriGenre.java
+++ simu/acp/ferme/TriGenre.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 public class TriGenre implements Comparator<Animal> {
     @Override
     public int compare(Animal a1, Animal a2) {
-        // false (male) < true (femelle) usually with Boolean.compare
         return Boolean.compare(a1.getGenre(), a2.getGenre());
     }
 }
