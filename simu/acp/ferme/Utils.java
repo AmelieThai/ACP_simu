0ca9b681e9dca7dc452081ad09bcf6bd6b819ab2
thamagicube <67897196+AmelieThai@users.noreply.github.com>
2026-01-01 23:51:23 +0100

    Add Utils class with constants for utility functions

---
+++ simu/acp/ferme/Utils.java
@@ -0,0 +1,9 @@
+package acp.ferme;
+
+import java.util.Random;
+
+public class Utils {
+    public static final double JOURS_PAR_AN = 365.25;
+    public static final Random rng = new Random();
+
+}
